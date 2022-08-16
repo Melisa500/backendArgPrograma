cfc9b13014399c3ba3e235d7899822b580aa7676
Melisa500 <melisabarrera24@gmail.com>
2022-08-16 19:03:09 -0300

    backend2

--- src/main/java/com/melisaportfolio/melisa/Controller/PersonaController.java
+++ src/main/java/com/melisaportfolio/melisa/Controller/PersonaController.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.melisaportfolio.melisa.Controller;
 
-/**
- *
- * @author Melisa
- */
+import com.melisaportfolio.melisa.Entity.Usuario;
+import com.melisaportfolio.melisa.Interface.IUsuarioService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonaController {
-    
+   @Autowired IUsuarioService iusuarioService; 
+   
+   @GetMapping("usuario")
+   public List<Usuario> getUsuario() {
+       return iusuarioService.getUsuario();
+    }
+   
+   @PostMapping("usuario")
+   public String createUsuario (@RequestBody Usuario usuario){
+       iusuarioService.saveUsuario(usuario);
+       return "Se creó un nuevo usuario";
+   }
+   
+   @DeleteMapping("usuario/{id}")
+   public String deleteUsuario(@PathVariable Long id){
+       iusuarioService.deleteUsuario(id);
+       return "Se borro el usuario";
+   }
+   
+   @PutMapping("usuario/{id}")
+   public Usuario editUsuario(@PathVariable Long id,@RequestBody Usuario usuario){
+      Usuario editUsuario = iusuarioService.findUsuario(id);
+      
+      editUsuario.setNombre(usuario.getNombre());
+      editUsuario.setApellido(usuario.getApellido());
+      editUsuario.setTitulo(usuario.getTitulo());
+      editUsuario.setSobre_mi(usuario.getSobre_mi());
+      editUsuario.setUrl_foto(usuario.getUrl_foto());
+      editUsuario.setUsername(usuario.getUsername());
+      editUsuario.setPassword(usuario.getPassword());
+      
+      iusuarioService.saveUsuario(editUsuario);
+      return editUsuario;
+   }
+   
+   
+   
 }
